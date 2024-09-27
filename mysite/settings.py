779b312943e42a9d43d51a81edbb2d4e5e131f9a
acer <ilkanbayiroglu@gmail.com>
2024-09-27 21:47:36 +0300

    -m

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,4 +129,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ilkanpython@gmail.com'
-EMAIL_HOST_PASSWORD = 'ycdyepouuglessmz'
+EMAIL_HOST_PASSWORD = os.getenv("ILKAN_DJANGO_PASSWORD")
